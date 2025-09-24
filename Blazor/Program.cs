69b2a7608540e076633eb6a7fe1ac1f96cf1b921
Mark <markdirekte@gmail.com>
2025-09-24 23:20:02 +0200

    Add "https://suitedreams-api.mercantec.tech" to work with deployed api

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -19,7 +19,8 @@ public class Program
         // Læs API endpoint fra miljøvariabler eller brug default
         var envApiEndpoint = Environment.GetEnvironmentVariable("API_ENDPOINT");
         Console.WriteLine($"API ENV Endpoint: {envApiEndpoint}");
-        var apiEndpoint = envApiEndpoint ?? "https://localhost:8091";
+        //var apiEndpoint = envApiEndpoint ?? "https://localhost:8091";
+        var apiEndpoint = envApiEndpoint ?? "https://suitedreams-api.mercantec.tech";
         Console.WriteLine($"API Endpoint: {apiEndpoint}");
 
         // Registrer HttpClient til API service med konfigurerbar endpoint
